daddd97f5381948f1eef7adc58d4d0ef96051ebe
Bruno Xavier <bruno.xavier@issuu.com>
2025-01-16 19:39:05 +0000

    feat: Add 'tasklist' plugin to render checklists

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Markdown from "unplugin-vue-markdown/vite";
+import { tasklist } from "@mdit/plugin-tasklist";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
@@ -11,7 +12,13 @@ export default defineConfig({
     vue({
       include: [/\.vue$/, /\.md$/],
     }),
-    Markdown({}),
+    Markdown({
+      markdownItSetup(md) {
+        md.use(tasklist, {
+          disabled: false,
+        });
+      },
+    }),
     vueDevTools(),
   ],
   resolve: {
