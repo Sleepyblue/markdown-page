b1141da16d847a185f418ab5c8b8b63fbaf0d6ee
Bruno Xavier <bruno.abs@live.com.pt>
2025-01-18 20:00:59 +0000

    refactor: Improve page responsiveness

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <Header :showScrollToTop="showScrollToTop" />
+  <Header />
   <main>
     <Intro @scroll-past="handleScrollPast" />
+    <button v-if="showScrollToTop" @click="scrollToTop" aria-label="Scroll to the top">Top</button>
   </main>
-
 </template>
 
 <script setup lang="ts">
@@ -17,6 +17,67 @@ const showScrollToTop = ref(false);
 const handleScrollPast = (show: boolean) => {
   showScrollToTop.value = show;
 };
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
 </script>
 
-<style></style>
+<style>
+main {
+  display: flex;
+  flex-direction: column;
+
+  button {
+    position: sticky;
+    right: 0;
+    bottom: 12px;
+    background: none;
+    border: none;
+    font-size: 1.6rem;
+    color: var(--paper-text);
+    cursor: pointer;
+    padding: 0;
+    align-self: flex-end;
+  }
+
+  button::before,
+  button::after {
+    position: absolute;
+    font-size: 1.8rem;
+    bottom: 0;
+    opacity: 0;
+    transition: all 0.5s cubic-bezier(0.22, 1, 0.36, 1);
+
+  }
+
+  button::before {
+    content: "[";
+    left: 0;
+  }
+
+  button::after {
+    content: "]";
+    right: 0;
+  }
+
+  button:hover::before {
+    left: -10px;
+    opacity: 1;
+
+    @media screen and (max-width: 1024px) {
+      left: -8px;
+    }
+  }
+
+  button:hover::after {
+    right: -10px;
+    opacity: 1;
+
+    @media screen and (max-width: 1024px) {
+      right: -8px;
+    }
+  }
+}
+</style>
