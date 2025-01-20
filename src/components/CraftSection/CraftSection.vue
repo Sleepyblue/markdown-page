ca9ed8d556e019b3c04d34b64d116874828185b9
Bruno Xavier <bruno.abs@live.com.pt>
2025-01-20 15:46:20 +0000

    feat: Add 'Craft' section component

---
+++ src/components/CraftSection/CraftSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <section ref="section" id="craft-section">
+    <h2 id="craft">## Craft</h2>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import useSectionObserver from "../../composables/useSectionObserver";
+
+const section = ref(null);
+
+const emit = defineEmits<{
+  (e: "visible-section", value: string | null): void;
+}>();
+
+useSectionObserver({ section, sectionEmit: emit });
+</script>
+
+<style></style>
