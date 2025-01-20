65653d71bd09ae61d72bbc4434ccb7a12e5255cf
Bruno Xavier <bruno.abs@live.com.pt>
2025-01-20 15:45:59 +0000

    feat: Add 'Outline' section component

---
+++ src/components/OutlineSection/OutlineSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <section ref="section" id="outline-section">
+    <h2 id="outline">## Outline</h2>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import useSectionObserver from "../../composables/useSectionObserver";
+
+const section = ref(null);
+
+const emit = defineEmits<{
+  (e: "visible-section", value: string | null): void;
+}>();
+
+useSectionObserver({ section, sectionEmit: emit });
+</script>
+
+<style></style>
