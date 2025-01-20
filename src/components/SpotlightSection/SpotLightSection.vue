8207c788c056c1b641047ec88d770a35d60b3e45
Bruno Xavier <bruno.xavier@issuu.com>
2025-01-20 15:45:22 +0000

    refactor: Rename 'Intro' component to 'Spotlight'

---
+++ src/components/SpotlightSection/SpotLightSection.vue
@@ -0,0 +1,21 @@
+<template>
+  <section ref="section">
+    <h1>{ Frontend Developer }</h1>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ref, defineEmits } from "vue";
+import useSectionObserver from "../../composables/useSectionObserver";
+
+const section = ref<HTMLElement | null>(null);
+
+const emit = defineEmits<{
+  (e: "visible-section", value: string | null): void;
+  (e: "scroll-past", value: boolean): void;
+}>();
+
+useSectionObserver({ section, sectionEmit: emit, scrollTopEmit: emit, threshold: 0.15 });
+</script>
+
+<style></style>
