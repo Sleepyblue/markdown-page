8ff58ed191a1e77ef70383dc2499ad2ea6c23505
Bruno Xavier <bruno.abs@live.com.pt>
2025-01-24 21:07:39 +0000

    feat: Add search functionality to 'Craft' section

--- src/components/CraftSection/CraftSection.vue
+++ src/components/CraftSection/CraftSection.vue
@@ -1,171 +1,431 @@
 <template>
   <section ref="section" id="craft-section">
+    <h2 id="craft">## Craft</h2>
+    <div class="skills">
+      <p>Quickly check if I know</p>
+      <div class="skills-input">
+        <label>
+          <span aria-hidden="true">$&nbsp;</span>
+          <input type="search" ref="input" v-model="searchTerm" @keyup.enter="onSearch" @input="() => errorMessage = ''"
+            placeholder=" [ Search skill ]" minlength="3" pattern="^(?![0-9]+$).+$" required
+            aria-label="Search through my skillset" aria-describedby="search-error" />
+        </label>
+        <p id="search-error" class="error-message" aria-live="polite">
+          {{ errorMessage }}
+        </p>
+        <button @click="onSearch">Search</button>
+      </div>
+      <div class="skills-list" v-if="foundSkills.length || notFoundSkills.length">
+        <div class="found" v-if="foundSkills.length">
+          <span>✓ Found:</span>
+          <ul>
+            <li v-for="skill in foundSkills" :key="skill.skill">
+              <span>{{ skill.skill }}</span> {{ skill.description }}
+            </li>
+          </ul>
+        </div>
+        <div class="not-found" v-if="notFoundSkills.length">
+          <span>✗ Not found:</span>
+          <ul>
+            <li v-for="notFoundSkill in notFoundSkills" :key="notFoundSkill">
+              <span>{{ notFoundSkill }}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
     <CraftMarkdown />
   </section>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import useSectionObserver from "../../composables/useSectionObserver";
-import CraftMarkdown from "../../docs/CraftMarkdown.md"
+import CraftMarkdown from "../../docs/CraftMarkdown.md";
 
-const section = ref(null);
+type Skill = { skill: string; description: string };
+
+const section = ref<HTMLElement | null>(null);
+const input = ref<HTMLInputElement | null>(null);
+const skillsList = ref<Skill[]>([]);
+const searchTerm = ref("");
+const foundSkills = ref<Skill[]>([]);
+const notFoundSkills = ref<string[]>([]);
+const errorMessage = ref("")
 
 const emit = defineEmits<{
   (e: "visible-section", value: string | null): void;
 }>();
 
 useSectionObserver({ section, sectionEmit: emit });
-</script>
 
-<style>
-#craft-section .markdown-body {
-  counter-reset: item 0;
+onMounted(() => {
+  const skillsListElement = section.value?.querySelector(".markdown-body ul");
 
+  if (!skillsListElement) return;
 
-  details {
-    counter-increment: item;
-    border-top: 1px dashed var(--paper-text);
-    padding: 2rem 0;
+  skillsList.value = [...skillsListElement.children].map((li) => {
+    const [skill, description] = li.textContent?.split(":") || [];
+    return { skill, description };
+  });
+  skillsListElement.remove();
+});
 
-    &:last-child {
-      border-bottom: 1px dashed var(--paper-text);
-    }
+const permissiveSearch = (input: string) => {
+  const normalizeString = (str: string) =>
+    str.toLowerCase().replace(/[^a-z]|\s?\d+(\.\d+)*|(?:\s?(js|ts))$/g, "");
+  const noMatch: string[] = [];
 
-    summary {
-      display: grid;
-      grid-template-columns: 0.3fr 0.7fr;
-      align-items: end;
-      column-gap: clamp(2rem, 5vw + 1rem, 6rem);
-      cursor: pointer;
-      font-size: var(--font-size-h2);
-      font-weight: 200;
+  const normalizedInput = normalizeString(input);
 
-      @media(max-width: 1024px) {
-        align-items: start;
-      }
+  if (normalizedInput === "") {
+    return { matches: [], noMatch };
+  }
 
-      @media(max-width: 600px) {
-        display: flex;
-        flex-direction: column;
-        gap: 2rem;
-      }
-    }
+  let matches = skillsList.value.filter(({ skill }) =>
+    normalizeString(skill).startsWith(normalizedInput),
+  );
 
-    /* Lacks some customization options */
-    summary::marker {
-      display: none;
-    }
+  if (matches.length > 0) {
+    matches = matches.sort(
+      (a, b) =>
+        normalizeString(a.skill).length - normalizeString(b.skill).length,
+    );
 
-    summary::before {
-      content: "\\0" counter(item);
-      font-size: var(--font-size-before);
-      line-height: 1;
-      font-weight: 200;
-      max-width: max-content;
-    }
+    const shortestLength = normalizeString(matches[0].skill).length;
+    matches = matches.filter(
+      ({ skill }) => normalizeString(skill).length === shortestLength,
+    );
+  }
 
-    summary span {
-      position: relative;
-      max-width: max-content;
-      line-height: 1;
-      font-weight: 200;
+  if (matches.length === 0) {
+    noMatch.push(input);
+  }
 
-      &:hover,
-      &:focus {
-        color: var(--paper-highlighted-text);
-      }
+  return { matches, noMatch };
+};
 
-      &:before,
-      &:after {
-        content: "";
-        position: absolute;
-        z-index: -1;
-        height: 100%;
-        top: 0;
-      }
+const onSearch = () => {
+  const inputElement = input.value;
 
-      &:before {
-        width: 3px;
-        left: 0;
-      }
+  if (inputElement) {
+    inputElement.setCustomValidity("");
 
-      &:after {
-        width: 3px;
-        right: 0;
-      }
+    switch (true) {
+      case inputElement.value === "":
+        inputElement.setCustomValidity("Input cannot be empty.");
+        errorMessage.value = "I think... you forgot to type something? Maybe? (¬‿¬)";
 
-      &:hover:before,
-      &:focus:before {
-        width: 50%;
-        height: 100%;
-        background-color: var(--paper-highlight);
-        transition: all 1s cubic-bezier(0.22, 1, 0.36, 1) 0s;
-      }
+        return;
+      case inputElement.validity.tooShort:
+        errorMessage.value = `C'mon, you gotta type at least ${inputElement.minLength} letters! (╯°Д°)╯`;
+        return;
 
-      &:hover:after,
-      &:focus:after {
-        width: 50%;
-        height: 100%;
-        background-color: var(--paper-highlight);
-        transition: all 1s cubic-bezier(0.22, 1, 0.36, 1) 0s;
-      }
-    }
-
-    div {
-      display: grid;
-      grid-template-columns: 0.3fr 0.7fr;
-      align-items: start;
-      padding: 0 2rem;
-      column-gap: clamp(2rem, 5vw + 1rem, 6rem);
-      /* border-top: 1px solid var(--paper-text); */
-
-      @media(max-width: 1024px) {
-        grid-template-columns: 1fr;
-        gap: 2rem;
-      }
-
-      @media(max-width: 600px) {
-        padding: 0;
-      }
-
-
-
-      ul {
-        list-style: none;
-        display: flex;
-        flex-wrap: wrap;
-        font-family: var(--font-type-body);
-        grid-row: 1 / 3;
-
-
-        li,
-        a {
-          font-weight: 700;
-        }
-
-        li:not(:last-child)::after {
-          content: "\00a0\00a0";
-        }
-      }
-
-      p {
-        grid-column: 2 / 3;
-
-        @media(max-width: 1024px) {
-          grid-column: unset;
-        }
-      }
+      case !/^(?![0-9]+$).{3,}$/.test(inputElement.value):
+        errorMessage.value = "Just numbers? Nah, that won't work! (¬_¬) Try typing some letters too!";
+        return;
     }
   }
 
-  details[open] summary {
-    padding-bottom: 2rem;
+  const { matches, noMatch } = permissiveSearch(searchTerm.value);
+  const uniqueSkills = new Set([...foundSkills.value, ...matches]);
+  const uniqueNotFoundSkills = new Set([...notFoundSkills.value, ...noMatch]);
+  foundSkills.value = Array.from(uniqueSkills);
+  notFoundSkills.value = Array.from(uniqueNotFoundSkills);
 
-    &::before,
-    span {
-      font-weight: 500;
+  searchTerm.value = "";
+};
+</script>
+
+<style>
+#craft-section {
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-section);
+
+  .skills {
+    display: grid;
+    grid-template-columns: 0.3fr 0.7fr;
+    max-width: 100%;
+
+    @media (max-width: 768px) {
+      display: flex;
+      flex-direction: column;
+
+      p {
+        margin-bottom: 2rem;
+      }
     }
+
+    .skills-input {
+      position: relative;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0.2rem 0.4rem;
+      border-top: 1px solid var(--paper-text);
+      border-right: 1px dashed var(--paper-text);
+      border-left: 1px dashed var(--paper-text);
+      border-bottom: 1px solid var(--paper-text);
+      grid-column: 2/ 3;
+      grid-row: 1/ 2;
+      gap: 1rem;
+      padding: 0 1rem;
+
+      button {
+        text-wrap: nowrap;
+      }
+
+      button::before,
+      button::after {
+        content: none;
+      }
+
+      button:active {
+        transform: translateY(2px)
+      }
+
+      .error-message {
+        position: absolute;
+        bottom: 100%;
+        display: none;
+        color: var(--paper-accent);
+        font-size: 1.4rem;
+      }
+
+      span {
+        position: relative;
+      }
+
+      span::after {
+        position: absolute;
+        content: "|";
+        right: -4px;
+        top: 50%;
+        transform: translateY(-50%);
+        font-size: 1.8rem;
+        font-weight: 300;
+        line-height: normal;
+        animation: inputFakeBlink 1s step-end infinite;
+      }
+
+      label:has(input:not(:placeholder-shown)) span::after,
+      label:has(input:focus) span::after {
+        display: none;
+      }
+
+      label:has(input:invalid) {
+        outline: 2px solid var(--paper-accent);
+      }
+
+      input:focus {
+        outline: none;
+      }
+
+      label:has(input:invalid)~.error-message {
+        display: block;
+      }
+
+    }
+
+    .skills-list {
+      display: flex;
+      flex-direction: column;
+      grid-column: 2/ 3;
+      grid-row: 2/ 3;
+      border-right: 1px dashed var(--paper-text);
+      border-left: 1px dashed var(--paper-text);
+      border-bottom: 1px solid var(--paper-text);
+      gap: 3.2rem;
+      padding: 2rem;
+
+      ul {
+        list-style: "-\00a0" inside;
+        font-family: var(--font-type-body);
+      }
+
+      >.not-found li span {
+        text-decoration: line-through;
+      }
+
+      .found {
+        li span {
+          color: var(--paper-highlighted-text);
+          background: var(--paper-highlight);
+          font-family: "Fira Sans";
+        }
+      }
+    }
+
+    label {
+      display: inline-flex;
+      width: 100%;
+    }
+
+    input {
+      display: inline-block;
+      background-color: transparent;
+      border: none;
+      width: 100%;
+      font-family: var(--font-type-header);
+      color: var(--paper-text);
+      caret-color: var(--paper-text);
+    }
+  }
+
+  .markdown-body {
+    counter-reset: item 0;
+
+    details {
+      counter-increment: item;
+      border-top: 1px dashed var(--paper-text);
+      padding: 2rem 0;
+
+      &:last-child {
+        border-bottom: 1px dashed var(--paper-text);
+      }
+
+      summary {
+        display: grid;
+        grid-template-columns: 0.3fr 0.7fr;
+        align-items: end;
+        column-gap: clamp(2rem, 5vw + 1rem, 6rem);
+        cursor: pointer;
+        font-size: var(--font-size-h2);
+        font-weight: 200;
+
+        @media (max-width: 1024px) {
+          align-items: start;
+        }
+
+        @media (max-width: 600px) {
+          display: flex;
+          flex-direction: column;
+          gap: 2rem;
+        }
+      }
+
+      /* Lacks some customization options */
+      summary::marker {
+        display: none;
+      }
+
+      summary::before {
+        content: "\\0" counter(item);
+        font-size: var(--font-size-before);
+        line-height: 1;
+        font-weight: 200;
+        max-width: max-content;
+      }
+
+      summary span {
+        position: relative;
+        max-width: max-content;
+        line-height: 1;
+        font-weight: 200;
+
+        &:hover,
+        &:focus {
+          color: var(--paper-highlighted-text);
+        }
+
+        &:before,
+        &:after {
+          content: "";
+          position: absolute;
+          z-index: -1;
+          height: 100%;
+          top: 0;
+        }
+
+        &:before {
+          width: 3px;
+          left: 0;
+        }
+
+        &:after {
+          width: 3px;
+          right: 0;
+        }
+
+        &:hover:before,
+        &:focus:before {
+          width: 50%;
+          height: 100%;
+          background-color: var(--paper-highlight);
+          transition: all 1s cubic-bezier(0.22, 1, 0.36, 1) 0s;
+        }
+
+        &:hover:after,
+        &:focus:after {
+          width: 50%;
+          height: 100%;
+          background-color: var(--paper-highlight);
+          transition: all 1s cubic-bezier(0.22, 1, 0.36, 1) 0s;
+        }
+      }
+
+      div {
+        display: grid;
+        grid-template-columns: 0.3fr 0.7fr;
+        align-items: start;
+        padding: 0 2rem;
+        column-gap: clamp(2rem, 5vw + 1rem, 6rem);
+
+        @media (max-width: 1024px) {
+          grid-template-columns: 1fr;
+          gap: 2rem;
+        }
+
+        @media (max-width: 600px) {
+          padding: 0;
+        }
+
+        ul {
+          list-style: none;
+          display: flex;
+          flex-wrap: wrap;
+          font-family: var(--font-type-body);
+          grid-row: 1 / 3;
+
+          li,
+          a {
+            font-weight: 700;
+          }
+
+          li:not(:last-child)::after {
+            content: "\00a0\00a0";
+          }
+        }
+
+        p {
+          grid-column: 2 / 3;
+
+          @media (max-width: 1024px) {
+            grid-column: unset;
+          }
+        }
+      }
+    }
+
+    details[open] summary {
+      padding-bottom: 2rem;
+
+      &::before,
+      span {
+        font-weight: 500;
+      }
+    }
+  }
+}
+
+@keyframes inputFakeBlink {
+  50% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
   }
 }
 </style>
