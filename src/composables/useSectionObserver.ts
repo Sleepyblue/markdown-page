d4fd0d3b1cfd9653da85e924875c3b65cded05ed
Bruno Xavier <bruno.abs@live.com.pt>
2025-01-29 11:19:29 +0000

    refactor: Adjust 'useSectionObserver' to include footer emit

--- src/composables/useSectionObserver.ts
+++ src/composables/useSectionObserver.ts
@@ -2,19 +2,25 @@ import { onMounted, type Ref } from "vue";
 
 interface SectionObserverType {
   section: Ref<HTMLElement | null>;
-  sectionEmit: (event: "visible-section", value: string | null) => void;
+  sectionEmit?: (event: "visible-section", value: string | null) => void;
   scrollTopEmit?: (event: "scroll-past", value: boolean) => void;
-  threshold?: number;
+  footerEmit?: (event: "footer-emit", value: number | null) => void;
+  threshold?: number | number[];
 }
 
 export default function useSectionObserver({
   section,
   sectionEmit,
   scrollTopEmit,
+  footerEmit,
   threshold = 0.8,
 }: SectionObserverType) {
   const handleIntersection = (entries: IntersectionObserverEntry[]) => {
     const entry = entries[0];
+
+    if (footerEmit) {
+      footerEmit("footer-emit", entry.intersectionRatio);
+    }
 
     if (entry.isIntersecting && sectionEmit) {
       sectionEmit("visible-section", entry.target.id);
@@ -26,9 +32,10 @@ export default function useSectionObserver({
   };
 
   onMounted(() => {
+    const thresholdValue = Array.isArray(threshold) ? threshold : [threshold];
     const observer = new IntersectionObserver(handleIntersection, {
       root: null,
-      threshold,
+      threshold: thresholdValue,
     });
 
     if (section.value) observer.observe(section.value);
