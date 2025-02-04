d17cdd7052089148b0d8103ddca729d685172cf6
Bruno Xavier <bruno.abs@live.com.pt>
2025-02-04 11:16:07 +0000

    chore: Add markdown default exports

---
+++ src/content/index.ts
@@ -0,0 +1,6 @@
+export { default as HeaderMarkdown } from "./HeaderMarkdown.md";
+export { default as SpotlightMarkdown } from "./SpotlightMarkdown.md";
+export { default as OutlineMardown } from "./OutlineMarkdown.md";
+export { default as CraftMarkdown } from "./CraftMarkdown.md";
+export { default as OnDeskMarkdown } from "./OnDeskMarkdown.md";
+export { default as FooterMarkdown } from "./FooterMarkdown.md";
